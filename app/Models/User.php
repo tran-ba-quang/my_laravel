6f6d08d7b0cceca2c69023c9a10ec6f1a3cc0eb0
tran-ba-quang <tranbaquangoooo@oooogmailcom>
2022-05-11 10:18:29 +0700

    first commit

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,10 +7,15 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Spatie\Permission\Traits\HasRoles;
+
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
+
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasApiTokens, HasFactory, Notifiable, HasRoles;
 
     /**
      * The attributes that are mass assignable.
@@ -42,3 +47,6 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
     ];
 }
+
+$role = Role::create(['name' => 'writer']);
+$permission = Permission::create(['name' => 'edit articles']);
--- config/app.php
+++ config/app.php
@@ -1,4 +1,4 @@
-<?php
+    <?php
 
 return [
 
@@ -139,6 +139,8 @@ return [
         /*
          * Laravel Framework Service Providers...
          */
+        Spatie\Permission\PermissionServiceProvider::class,
+        
         Illuminate\Auth\AuthServiceProvider::class,
         Illuminate\Broadcasting\BroadcastServiceProvider::class,
         Illuminate\Bus\BusServiceProvider::class,
